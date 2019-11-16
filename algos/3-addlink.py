be73f7404312b6c5f573ee3df329f3dbef6db0f6
kelr <kyle.ruan@protonmail.com>
2019-11-15 21:44:53 -0500

    Iterative solution

--- algos/3-addlink.py
+++ algos/3-addlink.py
@@ -12,12 +12,17 @@ LL representation:
 
 """
 
+
 class Node(object):
   def __init__(self, x):
     self.val = x
     self.next = None
 
-
+""" 
+Both are O(N) where N is the size of the larger list or just the size of any if theyre the same. 
+    Space is O(N) where N is the size of the larger list or just the size of any if theyre the same 
+        since its a new node each iteration/recursion. Recursive solution stores state on the call stack.
+"""
 class Solution:
     def add_two(self, list1, list2):
         return self.add_two_recursive(list1, list2, 0)
@@ -62,6 +67,57 @@ class Solution:
             carry = 0
         return sum, carry
 
+    # Iterative solution
+    def add_two_iter(self, node1, node2):
+        curr1 = node1
+        curr2 = node2
+        carry = 0
+        prev_node = None
+        root = None
+
+        while curr1 is not None and curr2 is not None:
+            val, carry = self.sum_and_carry(curr1.val, curr2.val, carry)
+            new_node = Node(val)
+            if prev_node is not None:
+                prev_node.next = new_node
+            else:
+                root = new_node
+            prev_node = new_node
+            curr1 = curr1.next
+            curr2 = curr2.next
+
+
+        if curr1 is None:
+            val, carry = self.sum_and_carry(0, curr2.val, carry)
+            new_node = Node(val)
+            if prev_node is not None:
+                prev_node.next = new_node
+            else:
+                root = new_node
+            prev_node = new_node
+            curr2 = curr2.next
+
+        elif curr2 is None:
+            val, carry = self.sum_and_carry(curr1.val, 0, carry)
+            new_node = Node(val)
+            if prev_node is not None:
+                prev_node.next = new_node
+            else:
+                root = new_node
+            prev_node = new_node
+            curr1 = curr1.next
+
+
+        # last carry
+        if carry:
+            new_node = Node(carry)
+            if prev_node is not None:
+                prev_node.next = new_node
+
+        return root
+
+
+
 
 l1 = Node(9)
 l1.next = Node(9)
@@ -73,6 +129,11 @@ l2.next = Node(9)
 l2.next.next = Node(9)
 
 answer = Solution().add_two(l1, l2)
+while answer:
+  print(answer.val, end=' ')
+  answer = answer.next
+print()
+answer = Solution().add_two_iter(l1, l2)
 while answer:
   print(answer.val, end=' ')
   answer = answer.next
