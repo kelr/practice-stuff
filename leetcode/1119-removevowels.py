0454b566dcce0e5995eea6107ad8ac1c10d8baa3
kelr <kyle.ruan@protonmail.com>
2020-06-29 23:17:35 -0400

    Day 28-1

---
+++ leetcode/1119-removevowels.py
@@ -0,0 +1,9 @@
+# Loop through the string, output non vowel characters.
+# O(N) time, loops through entire input string
+# O(N) space, output string could be N chars if input has no vowels.
+def removeVowels(S) -> str:
+    out = []
+    for char in S:
+        if char not in ["a", "e", "i", "o", "u"]:
+            out.append(char)
+    return "".join(out)
